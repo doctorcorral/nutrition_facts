6de75eedb2cb3c07070739500159f1813f81ebb2
Ricardo C <ricardo@suggestic.com>
2018-07-23 21:30:58 -0500

    add nutrientData parameter to main NutritionFacts Widget

--- lib/nutrirender/nutrirender.dart
+++ lib/nutrirender/nutrirender.dart
@@ -1,35 +1,8 @@
 import 'package:flutter/material.dart';
 
-final nutrientData = {
-  "CA": {"amount": 37.5, "unit": "mg"},
-  "CHOCDF": {"amount": 342.1, "unit": "g"},
-  "CHOLE": {"amount": 16.561, "unit": "mg"},
-  "ENERC_KCAL": {"amount": 2135.9, "unit": "kcal"},
-  "FAT": {"amount": 67.51, "unit": "g"},
-  "FE": {"amount": 18.2, "unit": "mg"},
-  "FIBTG": {"amount": 12.90, "unit": "g"},
-  "FOLAC": {"amount": 385.0, "unit": "mcg"},
-  "K": {"amount": 676.7, "unit": "mg"},
-  "MG": {"amount": 133.07, "unit": "mg"},
-  "NA": {"amount": 1038.89, "unit": "mg"},
-  "NIA": {"amount": 19.49, "unit": "mg"},
-  "P": {"amount": 381.19, "unit": "mg"},
-  "PROCNT": {"amount": 35.99, "unit": "g"},
-  "RIBF": {"amount": 1.6, "unit": "mg"},
-  "SE": {"amount": 84.75, "unit": "mcg"},
-  "SUGAR": {"amount": 74.49, "unit": "g"},
-  "THIA": {"amount": 2.74, "unit": "mg"},
-  "VITA_IU": {"amount": 5.0, "unit": "IU"},
-  "VITB6A": {"amount": 0.11, "unit": "mg"},
-  "VITD": {"amount": 28.39, "unit": "IU"},
-  "VITK1": {"amount": 0.75, "unit": "mcg"},
-  "ZN": {"amount": 2.93, "unit": "mg"},
-  "SATFAT": {"amount": 4.2, "unit": "g"},
-  "TRANSFAT": {"amount": 4.2, "unit": "g"},
-  "FIBER": {"amount": 4.23456, "unit": "g"},
-};
-
 class NutritionFacts extends StatefulWidget {
+  final nutrientData;
+  NutritionFacts({@required this.nutrientData}) : assert(nutrientData != null);
   NutritionFactsState createState() =>
       NutritionFactsState(nutrientData: nutrientData);
 }
@@ -47,12 +20,12 @@ class NutritionFactsState extends State<NutritionFacts> {
         body: Container(
       padding: EdgeInsets.all(40.0),
       color: Colors.white,
-      child: nutrientWidget(),
+      child: nutrientWidget(nutrientData: nutrientData),
     ));
   }
 }
 
-Widget nutrientWidget() {
+Widget nutrientWidget({nutrientData}) {
   return Container(
     padding: EdgeInsets.all(1.0),
     decoration:
@@ -63,7 +36,7 @@ Widget nutrientWidget() {
       child: Column(
         children: <Widget>[
           nutriHeader(calories: 23, servings: 3, servingSize: "8 oz."),
-          nutrientValues(),
+          nutrientValues(nutrientData: nutrientData),
           footerCalories(),
         ],
       ),
@@ -71,7 +44,7 @@ Widget nutrientWidget() {
   );
 }
 
-Widget nutrientValues() {
+Widget nutrientValues({nutrientData}) {
   //final n = (1.3456).toStringAsFixed(2);
   //final s = double.parse("1.2345");
   final nutrientTypes = [
