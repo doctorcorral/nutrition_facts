58c2b330b032c86e9563bbe570903f3787e988d0
Ricardo C <ricardo@suggestic.com>
2018-07-24 17:38:27 -0500

    add propper constructor parameter to NutritionFacts

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:nutritionfacts/nutrirender/nutrirender.dart';
+//import 'package:nutrientfacts/nutrientfacts.dart';
+
+final nutrientData = {
+  "CA": {"amount": 37.5, "unit": "mg"},
+  "CHOCDF": {"amount": 342.1, "unit": "g"},
+  "CHOLE": {"amount": 16.561, "unit": "mg"},
+  "ENERC_KCAL": {"amount": 2135.9, "unit": "kcal"},
+  "FAT": {"amount": 67.51, "unit": "g"},
+  "FE": {"amount": 18.2, "unit": "mg"},
+  "FIBTG": {"amount": 12.90, "unit": "g"},
+  "FOLAC": {"amount": 385.0, "unit": "mcg"},
+  "K": {"amount": 676.7, "unit": "mg"},
+  "MG": {"amount": 133.07, "unit": "mg"},
+  "NA": {"amount": 1038.89, "unit": "mg"},
+  "NIA": {"amount": 19.49, "unit": "mg"},
+  "P": {"amount": 381.19, "unit": "mg"},
+  "PROCNT": {"amount": 35.99, "unit": "g"},
+  "RIBF": {"amount": 1.6, "unit": "mg"},
+  "SE": {"amount": 84.75, "unit": "mcg"},
+  "SUGAR": {"amount": 74.49, "unit": "g"},
+  "THIA": {"amount": 2.74, "unit": "mg"},
+  "VITA_IU": {"amount": 5.0, "unit": "IU"},
+  "VITB6A": {"amount": 0.11, "unit": "mg"},
+  "VITD": {"amount": 28.39, "unit": "IU"},
+  "VITK1": {"amount": 0.75, "unit": "mcg"},
+  "ZN": {"amount": 2.93, "unit": "mg"},
+  "SATFAT": {"amount": 4.2, "unit": "g"},
+  "TRANSFAT": {"amount": 4.2, "unit": "g"},
+  "FIBER": {"amount": 4.23456, "unit": "g"},
+};
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData.light(),
     title: "Nutritional Facts",
-    home: nutritionFacts(),
+    home: NutritionFacts(nutrientData: nutrientData),
   ));
-}
-
-class nutritionFacts extends StatefulWidget {
-  nutritionFactsState createState() =>
-      nutritionFactsState(nutritionData: {"FAT": 10.5});
-}
-
-class nutritionFactsState extends State<nutritionFacts> {
-  final nutritionData;
-
-  //Need to type it as Map<String,num>
-  nutritionFactsState({@required this.nutritionData})
-      : assert(nutritionData != null);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(40.0),
-      color: Colors.white,
-      child: Container(
-        padding: EdgeInsets.all(1.0),
-        decoration: BoxDecoration(
-            border: new Border.all(color: Colors.black, width: 2.0)),
-        child: Container(
-          padding: EdgeInsets.all(2.0),
-          color: Colors.white,
-          child: Column(
-            children: <Widget>[
-              nutriHeader(),
-              nutrientValues(),
-              footerCalories(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
